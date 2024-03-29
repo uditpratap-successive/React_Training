a8174282a1b459853381ef9e4f4c735b8bc37af9
Udit Pratap <udit.pratap@uditpratapr-425.successivetech.com>
2024-03-29 10:49:07 +0530

    Feature/Day-1

---
+++ src/Day 1/Ques6/Buttonprop.jsx
@@ -0,0 +1,15 @@
+const bdata = [
+  {
+    text: "Button 1",
+    color: "red",
+  },
+  {
+    text: "Button 2",
+    color: "green",
+  },
+  {
+    text: "Button 3",
+    color: "#0000FF",
+  },
+];
+export default bdata;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom'
+import Assignment1 from './Day 1/Assignement1'
+
 
 ReactDOM.render(
-  <h1>This is the base app for the training</h1>,document.getElementById('root')
+  <> <Assignment1/>
+  </>,
+ document.getElementById('root')
 )
