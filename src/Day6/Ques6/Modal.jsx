c4f3aa28dfc06a05d807494a47796fe1221f8ae4
Udit Pratap <udit.pratap@uditpratapr-425.successivetech.com>
2024-04-22 11:43:56 +0530

    Updated assignment  6

--- src/Day6/Ques6/Modal.jsx
+++
@@ -1,4 +0,0 @@
-/*
-6.Create a React component (Modal) that displays a modal dialog with content. Users can open and close the modal. 
-Write tests to check if the modal opens and closes when triggered.*/
-
